712660f4530e9db7af07828e98d90e18ccca82bc
Dave Fetterman <fettermania@gmail.com>
2025-03-17 14:37:00 -0700

    Instructions, new case summaries

---
+++ scripts/retrieve_cases.sh
@@ -0,0 +1,240 @@
+#!/bin/bash
+# 1
+curl "https://www.finra.org/sites/default/files/fda_documents/2019060756501%20Robinhood%20Financial%2C%20LLC%20CRD%20165998%20and%20Robinhood%20Securities%2C%20LLC%20CRD%20287900%20AWC%20vr.pdf" > 2019060756501.pdf; pdftotext 2019060756501.pdf 2019060756501.txt
+
+# 2
+curl "https://www.finra.org/sites/default/files/fda_documents/2022075391301%20Robert%20W.%20Baird%20%26%20Co.%20Inc.%20CRD%208158%20Hefren-Tillotson%2C%20Inc.%20CRD%2053%20AWC%20vr.pdf" > 2022075391301.pdf; pdftotext 2022075391301.pdf 2022075391301.txt
+
+# 3
+curl "https://www.finra.org/sites/default/files/fda_documents/2019060753601%20Cova%20Capital%20Partners%20LLC%20CRD%20109761%20AWC%20lp.pdf" > 2019060753601.pdf; pdftotext 2019060753601.pdf 2019060753601.txt
+
+# 4
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490323%20Justin%20R.%20Deiter%20CRD%205225102%20AWC%20lp.pdf" > 2018056490323.pdf; pdftotext 2018056490323.pdf 2018056490323.txt
+
+# 5
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076855801%20IBN%20Financial%20Services%2C%20Inc.%20CRD%2042360%20%26%20Timothy%20E.%20Evans%20CRD%206259284%20AWC%20lp.pdf" > 2022076855801.pdf; pdftotext 2022076855801.pdf 2022076855801.txt
+
+# 6
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076855802%20Angelo%20J.%20Piccone%20CRD%201401761%20AWC%20lp.pdf" > 2022076855802.pdf; pdftotext 2022076855802.pdf 2022076855802.txt
+
+# 7
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070498106%20Alfred%20S.%20Vanderlaan%20CRD%201172406%20AWC%20gg.pdf" > 2021070498106.pdf; pdftotext 2021070498106.pdf 2021070498106.txt
+
+# 8
+curl "https://www.finra.org/sites/default/files/fda_documents/2022073340901%20Justin%20W.%20Pagel%20CRD%202554168%20AWC%20vr.pdf" > 2022073340901.pdf; pdftotext 2022073340901.pdf 2022073340901.txt
+
+# 9
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070498105%20Matthew%20I.%20Turner%20CRD%204114088%20AWC%20vr.pdf" > 2021070498105.pdf; pdftotext 2021070498105.pdf 2021070498105.txt
+
+# 10
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069377402%20Investments%20for%20You%2C%20Inc.%20CRD%2029257%20AWC%20vr.pdf" > 2021069377402.pdf; pdftotext 2021069377402.pdf 2021069377402.txt
+
+# 11
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076365001%20CIM%20Securities%2C%20LLC%20CRD%20120852%20AWC%20gg.pdf" > 2022076365001.pdf; pdftotext 2022076365001.pdf 2022076365001.txt
+
+# 12
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072094101%20Cambria%20Capital%2C%20LLC%20CRD%20133760%20AWC%20gg.pdf" > 2021072094101.pdf; pdftotext 2021072094101.pdf 2021072094101.txt
+
+# 13
+curl "https://www.finra.org/sites/default/files/fda_documents/2023079509301%20Mary%20Christine%20Beslagic%20CRD%205966835%20AWC%20lp.pdf" > 2023079509301.pdf; pdftotext 2023079509301.pdf 2023079509301.txt
+
+# 14
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072000902%20Union%20Capital%20Company%20CRD%20110301%20AWC%20gg.pdf" > 2021072000902.pdf; pdftotext 2021072000902.pdf 2021072000902.txt
+
+# 15
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069460001%20Independent%20Financial%20Group%2C%20LLC%20CRD%207717%20AWC%20vr.pdf" > 2021069460001.pdf; pdftotext 2021069460001.pdf 2021069460001.txt
+
+# 16
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490321%20Christopher%20P.%20Gallo%20CRD%206045888%20AWC%20vr.pdf" > 2018056490321.pdf; pdftotext 2018056490321.pdf 2018056490321.txt
+
+# 17
+curl "https://www.finra.org/sites/default/files/fda_documents/2023080042101%20CUNA%20Brokerage%20Services%2C%20Inc.%20CRD%2013941%20AWC%20lp.pdf" > 2023080042101.pdf; pdftotext 2023080042101.pdf 2023080042101.txt
+
+# 18
+curl "https://www.finra.org/sites/default/files/fda_documents/2023077080601%20ACP%20Securities%2C%20LLC%20CRD%20139049%20AWC%20lp.pdf" > 2023077080601.pdf; pdftotext 2023077080601.pdf 2023077080601.txt
+
+# 19
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070498104%20Linda%20J.%20Wimsatt%20CRD%201401802%20AWC%20lp.pdf" > 2021070498104.pdf; pdftotext 2021070498104.pdf 2021070498104.txt
+
+# 20
+curl "https://www.finra.org/sites/default/files/fda_documents/2022073262401%20G.%20research%2C%20LLC%20%20CRD%207353%20AWC%20lp.pdf" > 2022073262401.pdf; pdftotext 2022073262401.pdf 2022073262401.txt
+
+# 21
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490319%20Jesse%20D.%20Krapf%20CRD%205467277%20AWC%20lp.pdf" > 2018056490319.pdf; pdftotext 2018056490319.pdf 2018056490319.txt
+
+# 22
+curl "https://www.finra.org/sites/default/files/fda_documents/2023077597701%20Victor%20Alan%20Lessinger%20CRD%20830821%20AWC%20gg.pdf" > 2023077597701.pdf; pdftotext 2023077597701.pdf 2023077597701.txt
+
+# 23
+curl "https://www.finra.org/sites/default/files/fda_documents/2022074096804%20Investment%20Network%2C%20Inc.%20CRD%20127724%20and%20Gary%20L.%20Arnold%20CRD%20852859%20AWC%20vr.pdf" > 2022074096804.pdf; pdftotext 2022074096804.pdf 2022074096804.txt
+
+# 24
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069380601%20Blaine%20R.%20Stahlman%20CRD%201189213%20AWC%20gg.pdf" > 2021069380601.pdf; pdftotext 2021069380601.pdf 2021069380601.txt
+
+# 25
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076795001%20George%20Snyder%20IV%20CRD%204276539%20AWC%20lp.pdf" > 2022076795001.pdf; pdftotext 2022076795001.pdf 2022076795001.txt
+
+# 26
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490318%20George%20Apolonides%20aka%20George%20Apollo%20CRD%203101938%20AWC%20lp.pdf" > 2018056490318.pdf; pdftotext 2018056490318.pdf 2018056490318.txt
+
+# 27
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490316%20Kostas%20Tsamos%20aka%20Gus%20Tsamos%20CRD%202782014%20AWC%20lp.pdf" > 2018056490316.pdf; pdftotext 2018056490316.pdf 2018056490316.txt
+
+# 28
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490317%20Kyle%20Infinite%20Manning%20CRD%204571317%20AWC%20lp.pdf" > 2018056490317.pdf; pdftotext 2018056490317.pdf 2018056490317.txt
+
+# 29
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069303201%20Signet%20Securities%2C%20LLC%20CRD%20154198%20AWC%20gg.pdf" > 2021069303201.pdf; pdftotext 2021069303201.pdf 2021069303201.txt
+
+# 30
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070875201%20Park%20Avenue%20Securities%20LLC%20CRD%2046173%20AWC%20vr.pdf" > 2021070875201.pdf; pdftotext 2021070875201.pdf 2021070875201.txt
+
+# 31
+curl "https://www.finra.org/sites/default/files/fda_documents/2023080627901%20Independent%20Financial%20Group%2C%20LLC%20CRD%207717%20AWC%20vr.pdf" > 2023080627901.pdf; pdftotext 2023080627901.pdf 2023080627901.txt
+
+# 32
+curl "https://www.finra.org/sites/default/files/fda_documents/2017056224501%20SpeedTrader%2C%20Inc.%2C%20CRD%20107403%20AWC%20gg.pdf" > 2017056224501.pdf; pdftotext 2017056224501.pdf 2017056224501.txt
+
+# 33
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069395701%20Dalmore%20Group%20LLC%20CRD%20136352%20AWC%20lp.pdf" > 2021069395701.pdf; pdftotext 2021069395701.pdf 2021069395701.txt
+
+# 34
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069353202%20Securities%20Research%2C%20Inc.%20of%20Florida%20CRD%20%206516%20AWC%20gg.pdf" > 2021069353202.pdf; pdftotext 2021069353202.pdf 2021069353202.txt
+
+# 35
+curl "https://www.finra.org/sites/default/files/fda_documents/2023080627902%20Richard%20Mireles%20CRD%205288651%20AWC%20geg.pdf" > 2023080627902.pdf; pdftotext 2023080627902.pdf 2023080627902.txt
+
+# 36
+curl "https://www.finra.org/sites/default/files/fda_documents/2020068655901%20Kyle%20William%20Chapman%20CRD%206303483%20AWC%20lp.pdf" > 2020068655901.pdf; pdftotext 2020068655901.pdf 2020068655901.txt
+
+# 37
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072167901%20Stewart%20Ginn%20CRD%204503197%20Offer%20gg.pdf" > 2021072167901.pdf; pdftotext 2021072167901.pdf 2021072167901.txt
+
+# 38
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076491301%20Christopher%20Reynolds%20CRD%205976029%20AWC%20geg.pdf" > 2022076491301.pdf; pdftotext 2022076491301.pdf 2022076491301.txt
+
+# 39
+curl "https://www.finra.org/sites/default/files/fda_documents/2022073329701%20JVM%20Securities%2C%20LLC%20CRD%20290327%20AWC%20vr.pdf" > 2022073329701.pdf; pdftotext 2022073329701.pdf 2022073329701.txt
+
+# 40
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070498103%20Kim%20R.%20Kunz%20CRD%20718618%20AWC%20vr.pdf" > 2021070498103.pdf; pdftotext 2021070498103.pdf 2021070498103.txt
+
+# 41
+curl "https://www.finra.org/sites/default/files/fda_documents/2023077092001%20The%20Jeffrey%20Matthews%20Financial%20Group%2C%20LLC.%20%20CRD%2041282%20AWC%20lp.pdf" > 2023077092001.pdf; pdftotext 2023077092001.pdf 2023077092001.txt
+
+# 42
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076162101%20Laura%20Casey%20CRD%202684465%20AWC%20vr.pdf" > 2022076162101.pdf; pdftotext 2022076162101.pdf 2022076162101.txt
+
+# 43
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069353201%20John%20Wigmore%20Reilly%20III%20CRD%202209377%20AWC%20gg.pdf" > 2021069353201.pdf; pdftotext 2021069353201.pdf 2021069353201.txt
+
+# 44
+curl "https://www.finra.org/sites/default/files/fda_documents/2018060263201%20SI%20Securities%2C%20LLC%20CRD%20170937%20AWC%20gg.pdf" > 2018060263201.pdf; pdftotext 2018060263201.pdf 2018060263201.txt
+
+# 45
+curl "https://www.finra.org/sites/default/files/fda_documents/2020067795501%20Merrill%20Lynch%2C%20Pierce%2C%20Fenner%20%26%20Smith%20Incorporated%20CRD%207691%20AWC%20gg.pdf" > 2020067795501.pdf; pdftotext 2020067795501.pdf 2020067795501.txt
+
+# 46
+curl "https://www.finra.org/sites/default/files/fda_documents/2022073708202%20Steve%20Allen%20Moise%20CRD%204995443%20AWC%20lp.pdf" > 2022073708202.pdf; pdftotext 2022073708202.pdf 2022073708202.txt
+
+# 47
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070498102%20Alan%20Mason%20CRD%201302190%20AWC%20gg.pdf" > 2021070498102.pdf; pdftotext 2021070498102.pdf 2021070498102.txt
+
+# 48
+curl "https://www.finra.org/sites/default/files/fda_documents/2019064935602%20Kayan%20Securities%2C%20Inc.%20CRD%20156175%20and%20Yong%20Soo%20Kim%20CRD%20%201747849%20AWC%20gg.pdf" > 2019064935602.pdf; pdftotext 2019064935602.pdf 2019064935602.txt
+
+# 49
+curl "https://www.finra.org/sites/default/files/fda_documents/202169350301%20Murray%20Securities%2C%20Inc.%20CRD%20142783%20AWC%20lp.pdf" > 202169350301.pdf; pdftotext 202169350301.pdf 202169350301.txt
+
+# 50
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069335701%20Robert%20Spaulding%20Gleason%20Jr.%20CRD%201415067%20AWC%20lp.pdf" > 2021069335701.pdf; pdftotext 2021069335701.pdf 2021069335701.txt
+
+# 51
+curl "https://www.finra.org/sites/default/files/fda_documents/2019062948102%20James%20Brett%20Stuart%20CRD%203022149%20Default%20Decision%20df.pdf" > 2019062948102.pdf; pdftotext 2019062948102.pdf 2019062948102.txt
+
+# 52
+curl "https://www.finra.org/sites/default/files/fda_documents/2022074025801%20Joseph%20C.%20Desapio%20CRD%205837553%20AWC%20gg.pdf" > 2022074025801.pdf; pdftotext 2022074025801.pdf 2022074025801.txt
+
+# 53
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490309%20Tory%20A.%20Duggins%20CRD%204556340%20AWC%20vr.pdf" > 2018056490309.pdf; pdftotext 2018056490309.pdf 2018056490309.txt
+
+# 54
+curl "https://www.finra.org/sites/default/files/fda_documents/2017052494701%20LPL%20Financial%20LLC%20CRD%206413%20AWC%20gg.pdf" > 2017052494701.pdf; pdftotext 2017052494701.pdf 2017052494701.txt
+
+# 55
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069362101%20RCM%20Securities%20CRD%20%2015548%20AWC%20gg.pdf" > 2021069362101.pdf; pdftotext 2021069362101.pdf 2021069362101.txt
+
+# 56
+curl "https://www.finra.org/sites/default/files/fda_documents/2022077257801%20Malay%20Kumar%20CRD%202482909%20AWC%20gg.pdf" > 2022077257801.pdf; pdftotext 2022077257801.pdf 2022077257801.txt
+
+# 57
+curl "https://www.finra.org/sites/default/files/fda_documents/2019060753505%20Troy%20Allen%20Orlando%20CRD%206055474%20AWC%20vr%20%282023-1703290794748%29.pdf" > 2019060753505.pdf; pdftotext 2019060753505.pdf 2019060753505.txt
+
+# 58
+curl "https://www.finra.org/sites/default/files/fda_documents/2018056490307%20Joao%20A.%20Pinto%20CRD%206298233%20AWC%20vr%20%282023-1703204397449%29.pdf" > 2018056490307.pdf; pdftotext 2018056490307.pdf 2018056490307.txt
+
+# 59
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072389001%20Christopher%20Booth%20Kennedy%20CRD%204498061%20Order%20Accepting%20Offer%20Of%20Settlement%20lp%20%282023-1702858793928%29.pdf" > 2021072389001.pdf; pdftotext 2021072389001.pdf 2021072389001.txt
+
+# 60
+curl "https://www.finra.org/sites/default/files/fda_documents/2019061852801%20Haywood%20Securities%20%28USA%29%20Inc.%20CRD%2042072%20AWC%20vr%20%282023-1702513197989%29.pdf" > 2019061852801.pdf; pdftotext 2019061852801.pdf 2019061852801.txt
+
+# 61
+curl "https://www.finra.org/sites/default/files/fda_documents/2022073332301%20Parsonex%20Capital%20Markets%2C%20LLC%20CRD%20169578%20AWC%20lp.pdf" > 2022073332301.pdf; pdftotext 2022073332301.pdf 2022073332301.txt
+
+# 62
+curl "https://www.finra.org/sites/default/files/fda_documents/2019062948102%20James%20Brett%20Stuart%20CRD%203022149%20Complaint%20gg.pdf" > 2019062948102.pdf; pdftotext 2019062948102.pdf 2019062948102.txt
+
+# 63
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072167901%20Stewart%20Ginn%20CRD%204503197%20Complaint%20gg.pdf" > 2021072167901.pdf; pdftotext 2021072167901.pdf 2021072167901.txt
+
+# 64
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076459305%20Nicholas%20Michael%20Caruso%20CRD%207301382%20AWC%20vr.pdf" > 2022076459305.pdf; pdftotext 2022076459305.pdf 2022076459305.txt
+
+# 65
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072389001%20Christopher%20Booth%20Kennedy%20CRD%204498061%20Formal%20Complaint%20gg.pdf" > 2021072389001.pdf; pdftotext 2021072389001.pdf 2021072389001.txt
+
+# 66
+curl "https://www.finra.org/sites/default/files/fda_documents/2023079691201%20Elba%20M.%20Nogueras%20CRD%204459340%20AWC%20vr.pdf" > 2023079691201.pdf; pdftotext 2023079691201.pdf 2023079691201.txt
+
+# 67
+curl "https://www.finra.org/sites/default/files/fda_documents/2021070851501%20Network%201%20Financial%20Securities%2C%20Inc.%20CRD%2013577%20and%20Michael%20R%20Molinaro%20CRD%202358346%20AWC%20lp.pdf" > 2021070851501.pdf; pdftotext 2021070851501.pdf 2021070851501.txt
+
+# 68
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076459303%20Monmouth%20Capital%20Management%20LLC%20CRD%20290248%20AWC%20vr.pdf" > 2022076459303.pdf; pdftotext 2022076459303.pdf 2022076459303.txt
+
+# 69
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069376801%20American%20Wealth%20Management%2C%20Inc.%20CRD%20No.%2025536%20AWC%20gg.pdf" > 2021069376801.pdf; pdftotext 2021069376801.pdf 2021069376801.txt
+
+# 70
+curl "https://www.finra.org/sites/default/files/fda_documents/2022076103701%20Axos%20Invest%20LLC%20CRD%20172393%20AWC%20gg.pdf" > 2022076103701.pdf; pdftotext 2022076103701.pdf 2022076103701.txt
+
+# 71
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069357901%20Highlander%20Capital%20Group%2C%20Inc.%20CRD%2019074%20AWC%20lp.pdf" > 2021069357901.pdf; pdftotext 2021069357901.pdf 2021069357901.txt
+
+# 72
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069277101%20Harpeth%20Securities%2C%20LLC%20CRD%20109821%20AWC%20lp.pdf" > 2021069277101.pdf; pdftotext 2021069277101.pdf 2021069277101.txt
+
+# 73
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069380201%20DMK%20Advisor%20Group%2C%20Inc.%20CRD%20%2041067%20AWC%20gg.pdf" > 2021069380201.pdf; pdftotext 2021069380201.pdf 2021069380201.txt
+
+# 74
+curl "https://www.finra.org/sites/default/files/fda_documents/2020065599101%20SW%20Financial%20CRD%20145012%20awc%20vr.pdf" > 2020065599101.pdf; pdftotext 2020065599101.pdf 2020065599101.txt
+
+# 75
+curl "https://www.finra.org/sites/default/files/fda_documents/2021071134401%20Jose%20M.%20Candelario%20Padilla%20AWC%20lp%20%282023-1681086005178%29.pdf" > 2021071134401.pdf; pdftotext 2021071134401.pdf 2021071134401.txt
+
+# 76
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069365001%20Long%20Island%20Financial%20Group%2C%20Inc.%20%20CRD%2031148%20AWC%20gg.pdf" > 2021069365001.pdf; pdftotext 2021069365001.pdf 2021069365001.txt
+
+# 77
+curl "https://www.finra.org/sites/default/files/fda_documents/2020065683301%20Todd%20Anthony%20Cirella%20CRD%202396336%20AWC%20gg.pdf" > 2020065683301.pdf; pdftotext 2020065683301.pdf 2020065683301.txt
+
+# 78
+curl "https://www.finra.org/sites/default/files/fda_documents/2020065683302%20Edward%20Scott%20Short%20CRD%202462752%20AWC%20gg.pdf" > 2020065683302.pdf; pdftotext 2020065683302.pdf 2020065683302.txt
+
+# 79
+curl "https://www.finra.org/sites/default/files/fda_documents/2021069405501%20Charles%20V.%20Malico%20CRD%201507282%20AWC%20va%20%282022-1668126012623%29.pdf" > 2021069405501.pdf; pdftotext 2021069405501.pdf 2021069405501.txt
+
+# 80
+curl "https://www.finra.org/sites/default/files/fda_documents/2021072107601%20Aaron%20Capital%20Incorporated%20CRD%2028583%20AWC%20gg%20%282022-1656116424251%29.pdf" > 2021072107601.pdf; pdftotext 2021072107601.pdf 2021072107601.txt
