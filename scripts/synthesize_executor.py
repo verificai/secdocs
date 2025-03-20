688b674e74fe0ee1a0af378a25a9428da6367a45
Dave Fetterman <fettermania@gmail.com>
2025-03-20 14:52:03 -0700

    Adding three prompts

--- scripts/synthesize_executor.py
+++ scripts/synthesize_executor.py
@@ -1,7 +1,31 @@
 from synthesize_transactions import *
+import json
 
 #send_anthropic_message("Tell me a story", "You are a fantasy novelist who speaks in five word sentences.")
  
 case_data = get_case_data()
-print(case_data['Justin_W_Pagel_2022073340901.json'][0]['Demographics']['Age']
-)
+
+# TODO More than one plaintiff
+plaintiff_record = case_data['Justin_W_Pagel_2022073340901.json'][0]
+plaintiff_record_cleaned = select_relevant_keys(plaintiff_record)
+
+prompts = load_prompts()
+
+messages = []
+
+customer_prompt = prompts['customer.txt']
+customer_prompt_replaced = customer_prompt.replace("{CASE_DATA}", json.dumps(plaintiff_record_cleaned))
+
+customer_response = send_anthropic_message(messages, customer_prompt_replaced)
+
+allocation_prompt = prompts['allocation.txt']
+allocation_response = send_anthropic_message(messages, allocation_prompt)
+
+transactions_prompt = prompts['transactions.txt']
+transactions_response = send_anthropic_message(messages, transactions_prompt)
+
+print("** Customer Response: ", customer_response)
+print("** Allocation Response: ", allocation_response)
+print("** Transactions Response: ", transactions_response)
+
+
--- scripts/synthesize_transactions.py
+++ scripts/synthesize_transactions.py
@@ -26,16 +26,59 @@ API_URL = "https://api.anthropic.com/v1/messages"
 
 anthropic_client = anthropic.Anthropic(api_key=API_KEY)
 
-def send_anthropic_message(user_message, system_message="", max_tokens=4096):
-    messages = [{"role": "user", "content": user_message}]
+# def send_anthropic_message_raw(user_message, system_message="", max_tokens=4096):
+#     messages = [{"role": "user", "content": user_message}]
 
+#     response = anthropic_client.messages.create(
+#         model="claude-3-7-sonnet-20250219",
+#         system=system_message,
+#         max_tokens=max_tokens,
+#         messages=messages)
+    
+#     return response
+
+PROMPTS_DIR = "./synthesize_prompts"
+def load_prompts():
+    prompts_map = {}
+    # Read and process each JSON file in the directory
+    for filename in os.listdir(PROMPTS_DIR):
+        if filename.endswith(".txt"):
+            filepath = os.path.join(PROMPTS_DIR, filename)
+            with open(filepath, "r") as file:
+                data = file.read()
+
+            prompts_map[filename] = data
+    return prompts_map
+
+
+# Stateful (gross)
+def send_anthropic_message(messages, user_message, system_message="", max_tokens=4096):
+    new_user_message_record = {"role": "user", "content": user_message}
+
+    messages.append(new_user_message_record)
+    
     response = anthropic_client.messages.create(
         model="claude-3-7-sonnet-20250219",
         system=system_message,
         max_tokens=max_tokens,
         messages=messages)
 
-    return response.content[0].text
+    new_assistant_message_record =  {"role": "assistant", "content": response.content[0].text}
+    messages.append(new_assistant_message_record)
+    return messages[-1]['content'] # convenience
+
+
+
+def select_relevant_keys(plaintiff_record):
+    relevant_keys = [
+    "Demographics", 
+    "InvestmentType",
+    "Account Information"
+    "FinancialLossSuffered"
+    ]
+
+    filtered_record = {key: plaintiff_record[key] for key in relevant_keys if key in plaintiff_record }
+    return filtered_record
 
 
 def prepare_system_message():
